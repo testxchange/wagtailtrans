fa77207842720d9f97bb6db74709f2860013352d
Aleksandr Reshetniak <res.box@gmail.com>
2022-07-22 11:54:32 +0200

    Support Django 4 (fix)

--- src/wagtailtrans/wagtail_hooks.py
+++ src/wagtailtrans/wagtail_hooks.py
@@ -1,6 +1,5 @@
-from django.conf.urls import include, path
 from django.templatetags.static import static
-from django.urls import reverse
+from django.urls import include, path, reverse
 from django.utils.encoding import force_str
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
