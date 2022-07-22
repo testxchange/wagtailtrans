c91b658915eedcb00b32afcd38c440e944127fc4
Aleksandr Reshetniak <res.box@gmail.com>
2022-07-22 12:07:07 +0200

    Support Django 4 (fix)

--- src/wagtailtrans/urls/translations.py
+++ src/wagtailtrans/urls/translations.py
@@ -1,9 +1,9 @@
-from django.urls import re_url
+from django.urls import re_path
 
 from wagtailtrans.views.translation import TranslationView
 
 app_name = 'wagtailtrans'
 
 urlpatterns = [
-    re_url(r'^(?P<instance_id>\d+)/add/(?P<language_code>[^/]+)/$', TranslationView.as_view(), name='add'),
+    re_path(r'^(?P<instance_id>\d+)/add/(?P<language_code>[^/]+)/$', TranslationView.as_view(), name='add'),
 ]
